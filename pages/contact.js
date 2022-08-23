014609d8191a1b04f7075bae256b3f04d3e60f42
Megan Vest <mroxvest@icloud.com>
2022-08-23 14:49:14 -0500

    added form and updated css

--- pages/contact.js
+++ pages/contact.js
@@ -4,19 +4,42 @@ import logo from '../public/Logo.png'
 export default function ContactPage () {
     return (
     <div id="contact-page">
-        <header>
+        <div id="contact-left">
           <div id="logo">
               <Image src={logo} alt="Midwestern Interactive logo and MIDWESTERN in text" width={280} height={65} />
           </div>
-          <div id="home-link">
+          <div id="contact-text-row">
+                <h1>Heading One</h1>
+                <hr />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do dos eiusmod tempor incididunt ut labore et trace dolore magna aliqua.</p>
+                <p>Proin sagittis nisl rhoncus mattis rhoncus. At augue eget arcu dictum varius duis at consectetur lorem.</p>
+            </div>
+        </div>
+        <div id="contact-right">
+        <div id="home-link">
               <a href="\">home</a>
           </div>
-        </header>
-        <div id="contact-text-row">
-            <h1>Heading One</h1>
-            <hr />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do dos eiusmod tempor incididunt ut labore et trace dolore magna aliqua.</p>
-            <p>Proin sagittis nisl rhoncus mattis rhoncus. At augue eget arcu dictum varius duis at consectetur lorem.</p>
+            <div id="contact-form-row">
+                <h2>Heading Two</h2>
+                <form id="contact-form">
+                    <div className="textfield">
+                        <input type="text" placeholder="Input 1" />
+                    </div>
+                    <div className="textfield">
+                        <input type="text" placeholder="Input 2" />
+                    </div>
+                    <div className="textfield">
+                        <input type="text" placeholder="Input 3" />
+                    </div>
+                    <div className="textfield">
+                        <input type="text" placeholder="Input 4" />
+                    </div>
+                    <div className="textfield" id="message">
+                        <textarea placeholder="Textarea"></textarea>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     </div>
     );
