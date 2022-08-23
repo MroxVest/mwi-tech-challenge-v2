4a6e687e3b5ca1527228ad5e48d207b022505f7f
Megan Vest <mroxvest@icloud.com>
2022-08-23 13:15:23 -0500

    added heading one section

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,7 @@ import logo from '../public/Logo.png'
 
 export default function ContactPage () {
     return (
-    <div id="contact">
+    <div id="contact-page">
         <header>
           <div id="logo">
               <Image src={logo} alt="Midwestern Interactive logo and MIDWESTERN in text" width={280} height={65} />
@@ -12,6 +12,12 @@ export default function ContactPage () {
               <a href="\">home</a>
           </div>
         </header>
+        <div id="contact-text-row">
+            <h1>Heading One</h1>
+            <hr />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do dos eiusmod tempor incididunt ut labore et trace dolore magna aliqua.</p>
+            <p>Proin sagittis nisl rhoncus mattis rhoncus. At augue eget arcu dictum varius duis at consectetur lorem.</p>
+        </div>
     </div>
     );
 }
