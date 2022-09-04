62ce2ac02675e8b2d221284053ed04a3b489e30e
Megan Vest <mroxvest@icloud.com>
2022-09-03 20:29:16 -0500

    added pseudocode for api structure/notes

--- pages/api/content.js
+++ pages/api/content.js
@@ -1,2 +1,14 @@
 //Handle GET operations for the content
 // H1 vs H2, box1 vs box2/3, images
+
+// This gets called on every request
+export async function getServerSideProps() {
+    // Fetch data from external API - get correct link: use this page: https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/read-operations/project/
+    const res = await fetch(`https://.../data`)
+    const data = await res.json()
+  
+    // Pass data to the page via props (list in API style of items needed image: content.image_path, etc.)
+    return { props: { data } }
+  }
+
+  //On main page replace content with same API style item name (content.image_path, etc.)
